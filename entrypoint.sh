c566b6dd220418bfb4995f69a0350cfc566403f1
Martynas <martynas@graphity.org>
2017-09-12 00:45:11 +0200

    su addgroup

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ DOCKER_GID=$(ls -aln /var/run/docker.sock  | awk '{print $4}')
 
 if ! getent group $DOCKER_GID; then
     echo "Creating group host-docker:$DOCKER_GID"
-    addgroup --gid $DOCKER_GID host-docker
+    su addgroup --gid $DOCKER_GID host-docker
 fi
 
 usermod -aG $DOCKER_GID $JUSER
