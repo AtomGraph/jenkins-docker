fee3deb244ddb5d244095c6cf045b4758ff88fc0
Martynas <martynas@atomgraph.com>
2019-06-16 22:37:16 +0200

    Added double quotes on variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,11 @@ JUSER="jenkins"
 
 DOCKER_GID=$(ls -aln /var/run/docker.sock  | awk '{print $4}')
 
-if ! getent group $DOCKER_GID; then
+if ! getent group "$DOCKER_GID"; then
     echo "Creating group host-docker:$DOCKER_GID"
-    addgroup --gid $DOCKER_GID host-docker
+    addgroup --gid "$DOCKER_GID" host-docker
 fi
 
-usermod -aG $DOCKER_GID $JUSER
+usermod -aG "$DOCKER_GID" "$JUSER"
 
-exec su $JUSER -c "/usr/local/bin/jenkins.sh"
+exec su "$JUSER" -c "/usr/local/bin/jenkins.sh"
